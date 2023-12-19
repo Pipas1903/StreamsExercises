ecadea96fce68d598bb140f7cf15ce05eb8641da
PedroBarge <pedro525tavares@gmail.com>
2023-12-19 11:26:54 +0000

    a

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,35 +1,39 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Exercises {
+    OrderRepository orderRepository = new OrderRepository();
+    ProductRepository productRepository = new ProductRepository();
 
     public static void main(String[] args) {
         Exercises exercises = new Exercises();
-        System.out.println(exercises.ex1());
-        System.out.println(exercises.ex2());
-        System.out.println(exercises.ex3());
-        System.out.println(exercises.ex4());
-        System.out.println(exercises.ex5());
-        System.out.println(exercises.ex6());
-        System.out.println(exercises.ex7());
-        System.out.println(exercises.ex8());
-        System.out.println(exercises.ex9());
-        System.out.println(exercises.ex10());
-        System.out.println(exercises.ex11());
-        System.out.println(exercises.ex12());
-        System.out.println(exercises.ex13());
-        System.out.println(exercises.ex14());
-        System.out.println(exercises.ex15());
+        System.out.println("1 " + exercises.ex1());
+        System.out.println("2 " + exercises.ex2());
+        System.out.println("3 " + exercises.ex3());
+        System.out.println("4 " + exercises.ex4());
+        System.out.println("5 " + exercises.ex5());
+        System.out.println("6 " + exercises.ex6());
+        System.out.println("7 " + exercises.ex7());
+        System.out.println("8 " + exercises.ex8());
+        System.out.println("9 " + exercises.ex9());
+        System.out.println("10 " + exercises.ex10());
+        System.out.println("11 " + exercises.ex11());
+        System.out.println("12 " + exercises.ex12());
+        System.out.println("13 " + exercises.ex13());
+        System.out.println("14 " + exercises.ex14());
+        System.out.println("15 " + exercises.ex15());
     }
 
     public List<Product> ex1() {
         // Obtain a list of products belonging to category “Books” with price > 100
-        ProductRepository productRepository = new ProductRepository();
         return productRepository.getAll()
                 .stream()
                 .filter(getProductPrice -> getProductPrice.getPrice() > 100)
@@ -39,7 +43,6 @@ public class Exercises {
 
     public List<Order> ex2() {
         // Obtain a list of orders with products belonging to category “Baby”
-        OrderRepository orderRepository = new OrderRepository();
         return orderRepository.getAll()
                 .stream()
                 .filter(searchProducts -> searchProducts.getProducts()
@@ -51,16 +54,9 @@ public class Exercises {
     public List<Product> ex3() {
         // Obtain a list of products with category = “Toys” and then apply 10% discount
         double discount = 1.1;
-        ProductRepository productRepository = new ProductRepository();
         return productRepository.getAll()
                 .stream()
                 .filter(searchProductsCategory -> searchProductsCategory.getCategory().equals("Toys"))
-                /*
-                .map(product -> {
-                    product.setPrice(product.getPrice()/discount);
-                    return product;
-                })
-                */
                 .peek(product -> product.setPrice(product.getPrice() / discount))
                 .collect(Collectors.toList())
                 ;
@@ -68,41 +64,91 @@ public class Exercises {
 
     public List<Product> ex4() {
         // Obtain a list of products ordered by customers of tier 2 between 01-Feb-2021 and 01-Apr-2021
-        return null;
+        LocalDate inicialDate = LocalDate.of(2021, 2, 1);
+        LocalDate finalDate = LocalDate.of(2021, 4, 1);
+
+        List<Product> listaOfProducts = orderRepository.getAll()
+                .stream()
+                .filter(order -> order.getCustomer().getTier().equals(2)
+                        && order.getOrderDate().isAfter(inicialDate)
+                        && order.getOrderDate().isBefore(finalDate))
+                .flatMap(order -> order.getProducts().stream())
+                .distinct()
+                .toList();
+
+        return listaOfProducts;
     }
 
     public Optional<Product> ex5() {
         // Obtain the cheapest product from the “Books” category
-        return null;
+        return productRepository.getAll()
+                .stream()
+                .filter(category -> category.getCategory().equals("Books"))
+                .min(Comparator.comparingDouble(Product::getPrice))
+
+                ;
     }
 
     public List<Order> ex6() {
         // Obtain the 3 most recently placed orders
         // HELPFUL TIP: the method limit() might be useful here ;)
-        return null;
+        return orderRepository.getAll()
+                .stream()
+                .filter(limitDate -> limitDate.getOrderDate().isBefore(LocalDate.now()))
+                .sorted(Comparator.comparing(Order::getOrderDate).reversed())
+                .limit(3)
+                .collect(Collectors.toList())
+                ;
+
     }
 
     public List<Product> ex7() {
         // Obtain the list of orders which were ordered on 15-Mar-2021,
         // log each order to the console and then return their product list
         // HELPFUL TIP: the methods peek() and flatMap() might be useful here ;)
-        return null;
+        return orderRepository.getAll()
+                .stream()
+                .filter(date -> date.getOrderDate().isBefore(LocalDate.of(2021, 3, 15)))
+                .peek(order -> System.out.println(order.getOrderDate()))
+                .flatMap(products -> products.getProducts().stream())
+                .peek(product -> System.out.println(product.getName()))
+                .toList()
+                ;
+
     }
 
     public Double ex8() {
         // Calculate total sum of all orders placed in Feb 2021
-        return null;
+        return orderRepository.getAll()
+                .stream()
+                .filter(date -> date.getOrderDate().isAfter(LocalDate.of(2021, 1, 31))
+                        && date.getOrderDate().isBefore(LocalDate.of(2021, 3, 1)))
+                .flatMap(products -> products.getProducts().stream())
+                .map(Product::getPrice)
+                .reduce(0.0, Double::sum)
+                ;
     }
 
     public Double ex9() {
         // Calculate average order payment placed on 14-Mar-2021
-        return null;
+        return orderRepository.getAll()
+                .stream()
+                .filter(date -> date.getOrderDate().isEqual(LocalDate.of(2021, 3, 14)))
+                .flatMap(price -> price.getProducts().stream())
+                .map(Product::getPrice)
+                .collect(Collectors.averagingDouble(e->e))
+                ;
     }
 
     public DoubleSummaryStatistics ex10() {
         // Obtain a collection of statistic figures (i.e. sum, average, max, min, count)
         // for all products of category “Books”
         // HELPFUL TIP: the methods mapToDouble() and summaryStatistics() might be useful here ;)
+        productRepository.getAll()
+                .stream()
+                .filter(filterBook->filterBook.getCategory().equals("Books"))
+
+        ;
         return null;
     }
 
